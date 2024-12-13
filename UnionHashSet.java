f0cd167bc79cff94f2f6c3df332728c32eab9060
Tejasree-S27 <stejasree348@gmail.com>
2024-12-13 15:30:50 +0530

    Add files via upload

---
+++ UnionHashSet.java
@@ -0,0 +1,30 @@
+import java.util.*;
+ public class UnionHashSet 
+ {
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int arr1[]=new int[n];
+        for(int i=0;i<n;i++)
+            arr1[i]=sc.nextInt();
+        int m=sc.nextInt();
+        int arr2[]=new int[m];
+        for(int i=0;i<m;i++)
+            arr2[i]=sc.nextInt();
+        Set <Integer> a=new HashSet<>();
+        Set <Integer> b=new HashSet<>();
+        for(int s:arr1)
+        a.add(s);
+        for(int s1:arr2)
+        {
+        b.add(s1);
+        }
+        Set <Integer> u=new HashSet<>(a);
+        //union HashSet
+        u.addAll(b);
+        System.out.println(u);
+
+    }
+    
+}
