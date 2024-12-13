488ef02663129545cba34990896243c4f6f158cf
Tejasree-S27 <stejasree348@gmail.com>
2024-12-13 15:11:17 +0530

    Add files via upload

---
+++ HashMap.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class HashMap 
+{
+    public static void main(String args[])
+    {
+        HashMap<String,String> a=new HashMap<> ();
+        a.put("sno","01");
+        a.put("sname","Ram");
+        System.out.println(a);
+    }
+}
